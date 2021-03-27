2f9a5cfa429149ba5f44799485a14550163b23c3
Tonya Fletcher <tfletch4504@gmail.com>
2021-03-27 11:27:35 -0400

    lib folder

---
+++ index.js
@@ -0,0 +1,80 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
+const employees = [];
+
+function initGenerator() {
+    addHtml();
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Please enter the member name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Please enter the member position",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+        ],
+        name: "position"
+    },
+    {
+        message: "Please enter the member id",
+        name: "id",
+    },
+    {
+        message: "Please enter the member email",
+        name: "email"
+    }])
+        .then(function ({ name, position, id, email }) {
+            let positionInfo = "";
+            if (position === "Engineer") {
+                positionInfo = "Github username";
+            } else if (position === "Intern") {
+                positionInfo = "school name";
+            } else {
+                positionInfo = "office phone number";
+            }
+            inquirer.prompt([{
+                message: `Please enter member ${postionInfo}`,
+                name: "positionInfo"
+            },
+            {
+                type: "list",
+                message: "Would you like to add a member to this team?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name: "anotherMember"
+            }])
+                .then(function ({ positionInfo, anotherMember }) {
+                    let newMember;
+                    if (position === "Engineer") {
+                        newMember = new Engineer(name, id, email, positionInfo);
+                    } else if (position === "Intern") {
+                        newMember = new Intern(name, id, email, positionInfo);
+                    } else {
+                        newMember = new Manager(name, id, email, positionInfo)
+                    }
+                    employees.push(newMember);
+                    addHtml(newMemeber)
+                        .then(function () {
+                            if (anotherMemeber === "yes" {
+                                addMember();
+                            } else {
+                                completeHtml();
+                            }
+                        });
+
+                });
+        });
+}
