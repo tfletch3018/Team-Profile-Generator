5b56ddb1bfe15f977bc0db37b43428be32415578
Tonya Fletcher <tfletch4504@gmail.com>
2021-03-29 20:53:16 -0400

    manager test

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,31 +1,31 @@
 const Manager = require("../lib/manager");
 
-it("getRole() should return \"Intern\"", () => {
-    const testValue = "Intern";
-    const e = new Intern("Alice", 1, "test@test.com");
+it("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Alice", 1, "test@test.com");
     expect(e.getRole()).toBe(testValue);
   });
 
   it("Can get name via getName()", () => {
     const testValue = "Alice";
-    const e = new Intern(testValue);
+    const e = new Manager(testValue);
     expect(e.getName()).toBe(testValue);
   });
 
   it("Can get id via getId()", () => {
     const testValue = 100;
-    const e = new Intern("Foo", testValue);
+    const e = new Manager("Foo", testValue);
     expect(e.getId()).toBe(testValue);
   });
 
   it("Can set email via constructor argument", () => {
     const testValue = "test@test.com";
-    const e = new Intern("Foo", 1, testValue);
+    const e = new Manager("Foo", 1, testValue);
     expect(e.email).toBe(testValue);
   });
 
-  it("Can get school info via getSchool()", () => {
-    const testValue = "SchoolName";
-    const e = new Intern(testValue);
+  it("Can get office phone number via getOfficeNumber()", () => {
+    const testValue = "OfficeNumber";
+    const e = new Manager(testValue);
     expect(e.getName()).toBe(testValue);
   });
