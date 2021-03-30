09a21677d39f0c384655ed38b5bc4abe577ed993
Tonya Fletcher <tfletch4504@gmail.com>
2021-03-29 20:13:19 -0400

    employee tests passed

---
+++ test/Employee.test.js
@@ -0,0 +1,43 @@
+const Employee = require("../lib/employee");
+describe("Employee", () => {
+    it("Can instantiate Employee instance", () => {
+        const e = new Employee();
+        expect(typeof(e)).toBe("object");
+      });
+
+      it("Can set name via constructor arguments", () => {
+        const name = "Alice";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
+      });
+
+      it("Can set id via constructor argument", () => {
+        const testValue = 100;
+        const e = new Employee("Foo", testValue);
+        expect(e.id).toBe(testValue);
+      });
+
+      it("Can set email via constructor argument", () => {
+        const testValue = "test@test.com";
+        const e = new Employee("Foo", 1, testValue);
+        expect(e.email).toBe(testValue);
+      });
+
+      it("Can get name via getName()", () => {
+        const testValue = "Alice";
+        const e = new Employee(testValue);
+        expect(e.getName()).toBe(testValue);
+      });
+
+      it("Can get id via getId()", () => {
+        const testValue = 100;
+        const e = new Employee("Foo", testValue);
+        expect(e.getId()).toBe(testValue);
+      });
+      
+      it("getRole() should return \"Employee\"", () => {
+        const testValue = "Employee";
+        const e = new Employee("Alice", 1, "test@test.com");
+        expect(e.getRole()).toBe(testValue);
+      });
+    });
