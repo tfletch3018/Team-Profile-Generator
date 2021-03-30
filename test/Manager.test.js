b50fe535618afd5cc89e548bae22bece2be13378
Tonya Fletcher <tfletch4504@gmail.com>
2021-03-29 20:48:42 -0400

    intern test

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,31 @@
 const Manager = require("../lib/manager");
+
+it("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Alice", 1, "test@test.com");
+    expect(e.getRole()).toBe(testValue);
+  });
+
+  it("Can get name via getName()", () => {
+    const testValue = "Alice";
+    const e = new Intern(testValue);
+    expect(e.getName()).toBe(testValue);
+  });
+
+  it("Can get id via getId()", () => {
+    const testValue = 100;
+    const e = new Intern("Foo", testValue);
+    expect(e.getId()).toBe(testValue);
+  });
+
+  it("Can set email via constructor argument", () => {
+    const testValue = "test@test.com";
+    const e = new Intern("Foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+  });
+
+  it("Can get school info via getSchool()", () => {
+    const testValue = "SchoolName";
+    const e = new Intern(testValue);
+    expect(e.getName()).toBe(testValue);
+  });
